72b08d2f502bd2278800b5500a9a2ebe6584ecff
Fingolfin123 <81601678+Fingolfin123@users.noreply.github.com>
2025-05-24 11:36:30 -0400

    added pre-built notebooks

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from src.logger import logging
 
 def error_msg_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -12,8 +13,16 @@ def error_msg_detail(error,error_detail:sys):
 class CustomeException(Exception):
     def __init__(self,error_msg,error_detail:sys):
         super().__init__(error_msg)
-        self.error_msg=error_msg_detail(error_msg,error_detail=error_detail)
+        self.error_msg=error_msg_detail(error=error_msg,error_detail=error_detail)
 
     def __str__(self):
         return self.error_msg
     
+
+if __name__=="__main__":
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by Zero")
+        raise CustomeException(e,sys)
